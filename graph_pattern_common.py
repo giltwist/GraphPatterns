b12b77ab5e51de11ce7eabc782cfc3d8c3fcd105
giltwist <giltwist@gmail.com>
2025-11-15 15:57:19 -0500

    Beginning to modularize functions

---
+++ graph_pattern_common.py
@@ -0,0 +1,93 @@
+import snap
+import re
+
+
+def load_graph(filepath):
+    G = snap.LoadEdgeListStr(snap.TNGraph, filepath, 0, 1)
+    print("Number of Nodes: %d" % G.GetNodes())
+
+    G = snap.LoadEdgeListStr(snap.TUNGraph, filepath, 0, 1)
+    print("Number of Nodes: %d" % G.GetNodes())
+
+    G = snap.LoadEdgeListStr(snap.TNEANet, filepath, 0, 1)
+    print("Number of Nodes: %d" % G.GetNodes())
+
+    (G, Map) = snap.LoadEdgeListStr(snap.TNGraph, filepath, 0, 1, True)
+    print("Number of Nodes: %d" % G.GetNodes())
+    print("Number of Nodes: %d" % Map.Len())
+
+    # Basic testing of graph data
+    # convert input string to node id
+    NodeId = Map.GetKeyId("1065")
+    # convert node id to input string
+    NodeName = Map.GetKey(NodeId)
+    print("name", NodeName)
+    print("id  ", NodeId)
+
+
+# adapted from https://snap.stanford.edu/data/web-Amazon.html
+def parse_metadata(filepath):
+    try:
+        with open(filepath, "r") as f:
+            entry = {}
+            for l in f:
+                l = l.strip()
+
+                colonPos = l.find(":")
+
+                # End of dict entry found
+                if colonPos == -1:
+
+                    # Special Cases
+                    if l == "discontinued product":
+                        entry["discontinued"] = True
+
+                    if len(entry) > 0:
+                        if "Id" in entry:
+                            yield entry
+                        entry = {}
+                    continue
+
+                # remove extra spaces
+                l = re.sub(" +", " ", l)
+
+                eName = l[:colonPos]
+                rest = l[colonPos + 2 :]
+
+                # Would use switch but Python 3.9 doesn't have
+                if eName == "similar":
+                    temp = rest.split(" ")
+                    if int(temp[0]) > 0:
+                        entry[eName] = temp[1:]
+                elif eName == "categories":
+                    rows = int(rest)
+                    categories = []
+                    for i in range(rows):
+                        # TODO: Consider parsing categories into a tree
+                        l = next(f).strip()
+                        categories.append(l)
+                    entry[eName] = categories
+                elif eName == "reviews":
+                    rows = int(rest.split(" ")[1])
+                    reviews = []
+                    for i in range(rows):
+                        review = {}
+                        l = next(f).strip()
+                        l = re.sub(" +", " ", l)
+                        # fix persistent typo in data
+                        l = re.sub("cutomer", "customer", l)
+                        temp = l.split(" ")
+
+                        review["date"] = temp[0]
+                        for j in range(4):
+                            review[temp[2 * j + 1]] = (
+                                temp[2 * (j + 1)] if j == 0 else int(temp[2 * (j + 1)])
+                            )
+                        reviews.append(review)
+                    entry[eName] = reviews
+
+                else:
+                    entry[eName] = rest
+            yield entry
+    except:
+        print("ERROR: File not found")
--- graph_patterns.py
+++ graph_patterns.py
@@ -1,113 +1,27 @@
-import snap
 import os
-import re
-
 import simplejson
+from .graph_pattern_common import parse_metadata, load_graph
 
+# From https://snap.stanford.edu/data/amazon0601.html
+GRAPH_TEXT = "./data/amazon0601.txt"
 
+# From https://snap.stanford.edu/data/amazon-meta.html
+GRAPH_META = "./data/amazon-meta.txt"
 
-GRAPH_TEXT="./data/amazon0601.txt"
-GRAPH_META="./data/amazon-meta.txt"
-
-def make_graph():
-    G = snap.LoadEdgeListStr(snap.TNGraph, GRAPH_TEXT, 0, 1)
-    print("Number of Nodes: %d" % G.GetNodes())
-
-    G = snap.LoadEdgeListStr(snap.TUNGraph, GRAPH_TEXT, 0, 1)
-    print("Number of Nodes: %d" % G.GetNodes())
-
-    G = snap.LoadEdgeListStr(snap.TNEANet, GRAPH_TEXT, 0, 1)
-    print("Number of Nodes: %d" % G.GetNodes())
-
-    (G, Map) = snap.LoadEdgeListStr(snap.TNGraph, GRAPH_TEXT, 0, 1, True)
-    print("Number of Nodes: %d" % G.GetNodes())
-    print("Number of Nodes: %d" % Map.Len())
-
-    # convert input string to node id
-    NodeId = Map.GetKeyId("1065")
-    # convert node id to input string
-    NodeName = Map.GetKey(NodeId)
-    print("name", NodeName)
-    print("id  ", NodeId)
-
-# adapted from https://snap.stanford.edu/data/web-Amazon.html
-def parse(filename):
-  f = open(filename, 'r')
-  entry = {}
-  for l in f:
-    l = l.strip() 
-
-    colonPos = l.find(':')
-
-    #End of dict entry found
-    if colonPos == -1:
-      
-      #Special Cases
-      if l=="discontinued product":
-       entry['discontinued'] = True
-
-      if len(entry)>0:
-        if 'Id' in entry:
-          yield entry
-        entry = {}
-      continue   
-
-    # remove extra spaces
-    l = re.sub(' +', ' ',l)
-
-    eName = l[:colonPos]
-    rest = l[colonPos+2:]
-    
-
-    # Would use switch but Python 3.9 doesn't have
-    if eName == "similar":
-      temp = rest.split(" ")
-      if int(temp[0])>0:
-        entry[eName] = temp[1:]
-    elif eName == "categories":
-        rows = int(rest)
-        categories = []
-        for i in range(rows):
-          # TODO: Consider parsing categories into a tree
-          l = next(f).strip()
-          categories.append(l)
-        entry[eName] = categories
-    elif eName == "reviews":
-      rows = int(rest.split(" ")[1])
-      reviews = []
-      for i in range(rows):
-        review = {}
-        l = next(f).strip()
-        l = re.sub(' +', ' ',l)
-        # fix persistent typo in data
-        l = re.sub('cutomer', 'customer',l)
-        temp = l.split(" ")
-         
-        review['date']=temp[0]
-        for j in range(4):
-          review[temp[2*j+1]]=temp[2*(j+1)] if j==0 else int(temp[2*(j+1)])
-        reviews.append(review)
-      entry[eName] = reviews
-    
-    else:
-      entry[eName] = rest
-  yield entry
-
-
+# Graphs we are building
+GRAPH_CATEGORIES = "./data/amazon-categories.txt"
 
 if __name__ == "__main__":
-    if os.path.exists(GRAPH_TEXT):
-        #make_graph()
+    if os.path.exists(GRAPH_CATEGORIES):
         pass
     else:
-        print("Graph textFile not found")
+        print("Category data not found...generating")
 
-    if os.path.exists(GRAPH_META):
-        for i,e in enumerate(parse(GRAPH_META)):
-            if i<10:
-                print(str(i)+"|"+simplejson.dumps(e,indent=4)+"\n\n")
-            else:
-               break
-    else:
-        print("Graph metaFile not found")
-
+        if os.path.exists(GRAPH_META):
+            for i, e in enumerate(parse_metadata(GRAPH_META)):
+                if i < 10:
+                    print(str(i) + "|" + simplejson.dumps(e, indent=4) + "\n\n")
+                else:
+                    break
+        else:
+            print("ERROR: Graph metadata not found")
