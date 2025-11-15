75a09fa6565a54c0465d6bb0219deeaca4f7b40e
giltwist <giltwist@gmail.com>
2025-11-15 14:25:40 -0500

    Basic Parsing of metadata
    Possible TODO: Convert category breadcrumbs into a tree

--- graph_patterns.py
+++ graph_patterns.py
@@ -1,5 +1,6 @@
 import snap
 import os
+import re
 
 import simplejson
 
@@ -34,15 +35,62 @@ def parse(filename):
   f = open(filename, 'r')
   entry = {}
   for l in f:
-    l = l.strip()
+    l = l.strip() 
+
     colonPos = l.find(':')
+
+    #End of dict entry found
     if colonPos == -1:
-      yield entry
-      entry = {}
-      continue
+      
+      #Special Cases
+      if l=="discontinued product":
+       entry['discontinued'] = True
+
+      if len(entry)>0:
+        if 'Id' in entry:
+          yield entry
+        entry = {}
+      continue   
+
+    # remove extra spaces
+    l = re.sub(' +', ' ',l)
+
     eName = l[:colonPos]
     rest = l[colonPos+2:]
-    entry[eName] = rest
+    
+
+    # Would use switch but Python 3.9 doesn't have
+    if eName == "similar":
+      temp = rest.split(" ")
+      if int(temp[0])>0:
+        entry[eName] = temp[1:]
+    elif eName == "categories":
+        rows = int(rest)
+        categories = []
+        for i in range(rows):
+          # TODO: Consider parsing categories into a tree
+          l = next(f).strip()
+          categories.append(l)
+        entry[eName] = categories
+    elif eName == "reviews":
+      rows = int(rest.split(" ")[1])
+      reviews = []
+      for i in range(rows):
+        review = {}
+        l = next(f).strip()
+        l = re.sub(' +', ' ',l)
+        # fix persistent typo in data
+        l = re.sub('cutomer', 'customer',l)
+        temp = l.split(" ")
+         
+        review['date']=temp[0]
+        for j in range(4):
+          review[temp[2*j+1]]=temp[2*(j+1)] if j==0 else int(temp[2*(j+1)])
+        reviews.append(review)
+      entry[eName] = reviews
+    
+    else:
+      entry[eName] = rest
   yield entry
 
 
@@ -57,7 +105,7 @@ if __name__ == "__main__":
     if os.path.exists(GRAPH_META):
         for i,e in enumerate(parse(GRAPH_META)):
             if i<10:
-                print(simplejson.dumps(e))
+                print(str(i)+"|"+simplejson.dumps(e,indent=4)+"\n\n")
             else:
                break
     else:
