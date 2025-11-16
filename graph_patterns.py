b457265c6b71f658027576bd8ff84a5b9ffd4498
giltwist <giltwist@gmail.com>
2025-11-15 20:06:02 -0500

    Build the recommendations graph
    Also some cleanup

--- graph_patterns.py
+++ graph_patterns.py
@@ -1,6 +1,10 @@
 import os
-import simplejson
-from .graph_pattern_common import parse_metadata, load_graph
+import time
+from graph_pattern_common import parse_metadata
+
+import networkx as nx
+import matplotlib.pyplot as plt
+
 
 # From https://snap.stanford.edu/data/amazon0601.html
 GRAPH_TEXT = "./data/amazon0601.txt"
@@ -11,6 +15,7 @@ GRAPH_META = "./data/amazon-meta.txt"
 # Graphs we are building
 GRAPH_CATEGORIES = "./data/amazon-categories.txt"
 
+
 if __name__ == "__main__":
     if os.path.exists(GRAPH_CATEGORIES):
         pass
@@ -18,10 +23,47 @@ if __name__ == "__main__":
         print("Category data not found...generating")
 
         if os.path.exists(GRAPH_META):
+
+            graph = nx.Graph()
+
+            all_users=[]
+
+            start = time.time()
+
             for i, e in enumerate(parse_metadata(GRAPH_META)):
-                if i < 10:
-                    print(str(i) + "|" + simplejson.dumps(e, indent=4) + "\n\n")
-                else:
-                    break
-        else:
-            print("ERROR: Graph metadata not found")
+                #print(str(i) + "|" + simplejson.dumps(e, indent=4) + "\n\n")
+                
+                
+                asin = e['ASIN']
+                #print(i)
+                if 'reviews' in e:
+                    for r in e['reviews']:
+                        graph.add_edge(r['customer'],asin,weight = r['rating'])
+                        all_users.append(r['customer'])
+                if 'categories' in e:
+                    for c in e['categories']:
+                        #print(c)
+                        info = c.split("|")[1:]
+                        for i in range(len(info)-1):
+                            graph.add_edge(info[i],info[i+1])
+                        graph.add_edge(info[len(info)-1],asin)
+            
+            #deduplicate users
+            all_users=set(all_users)
+
+            #Remove any leaf nodes
+            leaves = [node for node,degree in dict(graph.degree()).items() if degree == 1]
+            multi_users=all_users-set(leaves)
+            print(len(multi_users))
+            graph.remove_nodes_from(leaves)
+
+            # Only for small numbers of nodes
+            #plt.figure(figsize=(60, 60)) 
+            #nx.draw(graph, with_labels=True, node_size=50)
+            #plt.savefig("test.png")
+
+            end = time.time()
+
+            print("\tElapsed time: ", end-start)
+
+            
