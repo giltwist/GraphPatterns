9ed9b7e0cc554d93d8de34dae5f728eb180c2013
giltwist <giltwist@gmail.com>
2025-11-25 15:08:04 -0500

    Clarifying edge and node types for GNN

--- graph_patterns.py
+++ graph_patterns.py
@@ -15,56 +15,81 @@ GRAPH_META = "./data/amazon-meta.txt"
 # Graphs we are building
 GRAPH_CATEGORIES = "./data/amazon-categories.txt"
 
+def generate_graph():
+
+    if os.path.exists(GRAPH_META):
+
+        graph = nx.Graph()
+
+        all_users=[]
+
+        start = time.time()
+
+        for i, e in enumerate(parse_metadata(GRAPH_META)):
+            #print(str(i) + "|" + simplejson.dumps(e, indent=4) + "\n\n")
+            
+            
+            asin = e['ASIN']
+            #print(i)
+            graph.add_node(asin,type='product')
+            if 'reviews' in e:
+                for r in e['reviews']:
+                    graph.add_node(r['customer'],type='user')
+                    
+                    graph.add_edge(r['customer'],asin,weight = r['rating'], type='review')
+                    all_users.append(r['customer'])
+            if 'categories' in e:
+                for c in e['categories']:
+                    #print(c)
+                    info = c.split("|")[1:]
+                    for i in range(len(info)-1):
+                        graph.add_node(info[i],type='category')
+                        graph.add_node(info[i+1],type='category')
+                        graph.add_edge(info[i],info[i+1],weight = 0, type='category_hierarchy')
+                    graph.add_edge(info[len(info)-1],asin,weight = 0, type='product_type')
+        
+        #deduplicate users
+        all_users=set(all_users)
+        print(f"Total users: {len(all_users)}")
+
+        #Remove any leaf nodes
+        print("Pruning singleton reviewers.")
+        leaves = [node for node,degree in dict(graph.degree()).items() if degree == 1]
+        multi_users=all_users-set(leaves)
+        print(f"Reviewers with multiple reviews: {len(multi_users)}.")
+        graph.remove_nodes_from(leaves)
+
+        end = time.time()
+
+        nx.write_graphml(graph,GRAPH_CATEGORIES)
+        print("\tElapsed time: ", end-start)
+        return graph
+    else:
+        print("Amazon metadata not found.  Download it from  https://snap.stanford.edu/data/amazon-meta.html")
+
+
+def visualize_graph(graph):
+    # Only for small numbers of nodes
+    plt.figure(figsize=(60, 60)) 
+    if (graph.number_of_nodes()<1000 and graph.number_of_edges()>1000):
+        
+        nx.draw(graph, with_labels=True, node_size=50)
+    else:
+        print("Graph too large to visualize, summarizing")
+        summary_graph=nx.snap_aggregation(graph, node_attributes=("type",))
+        nx.draw(summary_graph, with_labels=True, node_size=50)
+    plt.savefig("test.png")
+
+
 
 if __name__ == "__main__":
     if os.path.exists(GRAPH_CATEGORIES):
-        pass
+        print("Category graph found")
+        graph = nx.read_edgelist(GRAPH_CATEGORIES,nodetype=str, data=(('weight',int),('color',str)))
     else:
-        print("Category data not found...generating")
-
-        if os.path.exists(GRAPH_META):
-
-            graph = nx.Graph()
-
-            all_users=[]
-
-            start = time.time()
-
-            for i, e in enumerate(parse_metadata(GRAPH_META)):
-                #print(str(i) + "|" + simplejson.dumps(e, indent=4) + "\n\n")
-                
-                
-                asin = e['ASIN']
-                #print(i)
-                if 'reviews' in e:
-                    for r in e['reviews']:
-                        graph.add_edge(r['customer'],asin,weight = r['rating'])
-                        all_users.append(r['customer'])
-                if 'categories' in e:
-                    for c in e['categories']:
-                        #print(c)
-                        info = c.split("|")[1:]
-                        for i in range(len(info)-1):
-                            graph.add_edge(info[i],info[i+1])
-                        graph.add_edge(info[len(info)-1],asin)
-            
-            #deduplicate users
-            all_users=set(all_users)
-            print(f"Total users: {len(all_users)}")
-
-            #Remove any leaf nodes
-            leaves = [node for node,degree in dict(graph.degree()).items() if degree == 1]
-            multi_users=all_users-set(leaves)
-            print(f"Reviewers with multiple reviews: {len(multi_users)}")
-            graph.remove_nodes_from(leaves)
-
-            # Only for small numbers of nodes
-            #plt.figure(figsize=(60, 60)) 
-            #nx.draw(graph, with_labels=True, node_size=50)
-            #plt.savefig("test.png")
-
-            end = time.time()
-
-            print("\tElapsed time: ", end-start)
+        print("Category graph not found...generating")
+        graph = generate_graph()
+    
+    visualize_graph(graph)
 
             
