05bbaa66df09a56fb3f66226afa6a2b6b4e32611
giltwist <giltwist@gmail.com>
2025-11-26 18:09:41 -0500

    First successful full run with a metapath2vec GNN

--- graph_patterns.py
+++ graph_patterns.py
@@ -1,21 +1,41 @@
+
 import os
 import time
 from graph_pattern_common import parse_metadata
 
-import networkx as nx
-from stellargraph import StellarGraph
 import matplotlib.pyplot as plt
+from math import isclose
+from sklearn.decomposition import PCA
+import os
+import networkx as nx
+import numpy as np
+import pandas as pd
+from stellargraph import StellarGraph, datasets
+from stellargraph.data import EdgeSplitter
+from collections import Counter
+import multiprocessing
+from sklearn.model_selection import train_test_split
 
-# From https://snap.stanford.edu/data/amazon0601.html
-GRAPH_TEXT = "./data/amazon0601.txt"
+from stellargraph.data import UniformRandomMetaPathWalk
+from gensim.models import Word2Vec
+
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegressionCV
+from sklearn.metrics import roc_auc_score
+from sklearn.preprocessing import StandardScaler
+
+# == GLOBAL CONFIG ==
 
 # From https://snap.stanford.edu/data/amazon-meta.html
 GRAPH_META = "./data/amazon-meta.txt"
 
-# Graphs we are building
+# Graph we are building
 GRAPH_CATEGORIES = "./data/amazon-categories.txt"
 
+# 1 out of every X products will be included in the grap
+GRAPH_REDUCTION_FACTOR = 10
 
+# == END CONFIG == 
 
 def generate_graph():
 
@@ -32,25 +52,31 @@ def generate_graph():
             
             #Useful for debugging
             #print(str(i) + "|" + simplejson.dumps(e, indent=4) + "\n\n")
+
+            # NOTE: My desktop  didn't have enough memory (16GB) to do the whole dataset
+            # This if reduces dataset by GRAPH_REDUCTION_FACTOR to accommodate that
+            # If you have 32 or 64GB of memory, it may be worth trying lower factors
+            # NOTE: Time estimates were calculated with GRAPH_REDUCTION_FACTOR=2, expect double or quadruple time to do full dataset
+            if i%GRAPH_REDUCTION_FACTOR==0:
             
-            asin = e['ASIN']
-            #print(i)
-            graph.add_node(asin,type='product')
-            if 'reviews' in e:
-                for r in e['reviews']:
-                    graph.add_node(r['customer'],type='user')
-                    
-                    graph.add_edge(r['customer'],asin,weight = r['rating'], type='review')
-                    all_users.append(r['customer'])
-            if 'categories' in e:
-                for c in e['categories']:
-                    #print(c)
-                    info = c.split("|")[1:]
-                    for i in range(len(info)-1):
-                        graph.add_node(info[i],type='category')
-                        graph.add_node(info[i+1],type='category')
-                        graph.add_edge(info[i],info[i+1],weight = 0, type='category_hierarchy')
-                    graph.add_edge(info[len(info)-1],asin,weight = 0, type='product_type')
+                asin = e['ASIN']
+                graph.add_node(asin,type='product')
+                if 'reviews' in e:
+                    for r in e['reviews']:
+                        graph.add_node(r['customer'],type='user')
+                        
+                        graph.add_edge(r['customer'],asin,weight = r['rating'], type='review')
+                        all_users.append(r['customer'])
+                if 'categories' in e:
+                    for c in e['categories']:
+                        #print(c)
+                        info = c.split("|")[1:]
+                        for i in range(len(info)-1):
+                            graph.add_node(info[i],type='category')
+                            graph.add_node(info[i+1],type='category')
+                            graph.add_edge(info[i],info[i+1],weight = 0, type='category_hierarchy')
+                        graph.add_edge(info[len(info)-1],asin,weight = 0, type='product_type')
+                #TODO: Add product-to-product edges with the similar-to information.
         
         #deduplicate users
         all_users=set(all_users)
@@ -64,8 +90,8 @@ def generate_graph():
         graph.remove_nodes_from(leaves)
 
 
-        # NOTE: GraphML stored node types as well, but was 1.1GB instead of 300MB
-        # NOTE: Switched to edge list based on research that suggested it was more memory efficient
+        # NOTE: Tried GraphML first because it stored node types as well, but was 1.1GB
+        # NOTE: Switched to edge list based on research that suggested it was more memory efficient, reduced size to 300MB
         nx.write_edgelist(graph,GRAPH_CATEGORIES,delimiter='|', data=['weight','type'])
         return graph
     else:
@@ -81,12 +107,207 @@ def visualize_graph(graph):
     else:
         print("Graph too large to visualize")
 
+# == THIS SECTION COMES MORE OR LESS VERBATIM FROM A TUTORIAL ==
+# StellarGraph metapath2vec tutorial - https://stellargraph.readthedocs.io/en/latest/demos/link-prediction/metapath2vec-link-prediction.html
+
+
+def split_train_test(graph):
+
+    # Config options
+    dimensions = 128
+    num_walks = 1
+    walk_length = 10
+    context_window_size = 10
+    num_iter = 1
+    workers = multiprocessing.cpu_count()
+
+    # Review of same product OR of products that are no more than 3 category hops away
+    user_metapaths = [
+        ["user", "product", "user"],
+        ["user", "product", "category", "product", "user"],
+        ["user", "product", "category", "category", "product", "user"],
+        ["user", "product", "category", "category", "category", "product", "user"],
+        ["product","category","product"],
+        ["category","category"]
+    ]
+
+    # BEGIN HELPER FUNCTIONS
+
+    def metapath2vec_embedding(graph, name):
+        rw = UniformRandomMetaPathWalk(graph)
+        walks = rw.run(
+            graph.nodes(), n=num_walks, length=walk_length, metapaths=user_metapaths
+        )
+        print(f"Number of random walks for '{name}': {len(walks)}")
+
+        model = Word2Vec(
+            walks,
+            vector_size=dimensions,
+            window=context_window_size,
+            min_count=0,
+            sg=1,
+            workers=workers,
+            epochs=num_iter,
+        )
+
+        def get_embedding(u):
+            return model.wv[u]
+
+        return get_embedding
+
+
+    # 1. link embeddings
+    def link_examples_to_features(link_examples, transform_node, binary_operator):
+        return [
+            binary_operator(transform_node(src), transform_node(dst))
+            for src, dst in link_examples
+        ]
+
+
+    # 2. training classifier
+    def train_link_prediction_model(
+        link_examples, link_labels, get_embedding, binary_operator
+    ):
+        clf = link_prediction_classifier()
+        link_features = link_examples_to_features(
+            link_examples, get_embedding, binary_operator
+        )
+        clf.fit(link_features, link_labels)
+        return clf
+
+
+    def link_prediction_classifier(max_iter=2000):
+        lr_clf = LogisticRegressionCV(Cs=10, cv=10, scoring="roc_auc", max_iter=max_iter)
+        return Pipeline(steps=[("sc", StandardScaler()), ("clf", lr_clf)])
+
+
+    # 3. and 4. evaluate classifier
+    def evaluate_link_prediction_model(
+        clf, link_examples_test, link_labels_test, get_embedding, binary_operator
+    ):
+        link_features_test = link_examples_to_features(
+            link_examples_test, get_embedding, binary_operator
+        )
+        score = evaluate_roc_auc(clf, link_features_test, link_labels_test)
+        return score
+
+
+    def evaluate_roc_auc(clf, link_features, link_labels):
+        predicted = clf.predict_proba(link_features)
+
+        # check which class corresponds to positive links
+        positive_column = list(clf.classes_).index(1)
+        return roc_auc_score(link_labels, predicted[:, positive_column])
+
+    def operator_l1(u, v):
+        return np.abs(u - v)
+
+
+    def operator_l2(u, v):
+        return (u - v) ** 2
+
+
+    def run_link_prediction(binary_operator):
+        clf = train_link_prediction_model(
+            examples_train, labels_train, embedding_train, binary_operator
+        )
+        score = evaluate_link_prediction_model(
+            clf,
+            examples_model_selection,
+            labels_model_selection,
+            embedding_train,
+            binary_operator,
+        )
+
+        return {
+            "classifier": clf,
+            "binary_operator": binary_operator,
+            "score": score,
+        }
+    # END HELPER FUNCTIONS
+
+    start = time.time()
+    print('Sampling reviews and reviews-complement (est. ~3 minutes)')
+
+    edge_splitter_test = EdgeSplitter(graph)
+
+    # Randomly sample a fraction p=0.1 of all positive links, and same number of negative links, from graph, and obtain the
+    # reduced graph graph_test with the sampled links removed:
+    graph_test, examples_test, labels_test = edge_splitter_test.train_test_split(
+    p=0.1, method="global", edge_label="review"
+    )
+
+    end = time.time()
+    print(f"\tSampling time: {end-start}") 
+
+
+    start = time.time()
+    print('Building training set (est. ~5 minutes)')
+
+    edge_splitter_train = EdgeSplitter(graph_test, graph)
+    
+    graph_train, examples, labels = edge_splitter_train.train_test_split(
+    p=0.1, method="global", edge_label="review"
+    )
+    
+    (
+    examples_train,
+    examples_model_selection,
+    labels_train,
+    labels_model_selection,
+    ) = train_test_split(examples, labels, train_size=0.75, test_size=0.25)
+
+    end = time.time()
+    print(f"\t Building time: {end-start}") 
+
+    start = time.time()
+    print('Embedding training set (est. ~15 minutes)')
+    embedding_train = metapath2vec_embedding(graph_train, "Train Graph")
+    end = time.time()
+    print(f"\t Embedding time: {end-start}") 
+
+
+    binary_operators = [operator_l1, operator_l2]
+
+    results = [run_link_prediction(op) for op in binary_operators]
+    best_result = max(results, key=lambda result: result["score"])
+
+    print(f"Best result from '{best_result['binary_operator'].__name__}'")
+
+    start = time.time()
+    print('Embedding testing set (est. ~15 minutes)')
+    embedding_test = metapath2vec_embedding(graph_test, "Test Graph")
+    end = time.time()
+    print(f"\t Embedding time: {end-start}") 
+
+    start = time.time()
+    print('Embedding testing set (est. ~X minutes)')
+    test_score = evaluate_link_prediction_model(
+    best_result["classifier"],
+    examples_test,
+    labels_test,
+    embedding_test,
+    best_result["binary_operator"],
+    )
+    print(
+    f"ROC AUC score on test set using '{best_result['binary_operator'].__name__}': {test_score}"
+    )
+    end = time.time()
+    print(f"\t Evaluation time: {end-start}") 
+
+
+
+
+# == END TUTORIAL SECTION ==
+
+
+def estimate_progress(task,duration):
+    pass
 
 
 if __name__ == "__main__":
     if os.path.exists(GRAPH_CATEGORIES):
-        print("Category graph found")
-
+        print("\033[91m {}\033[00m".format("Loading existing category graph (est. ~30 seconds)"))
         start = time.time()
         nx_graph = nx.read_edgelist(GRAPH_CATEGORIES,delimiter='|',nodetype=str, data=(('weight',int),('type',str)),comments=None)
         #Restore node types not saved in edgelist
@@ -98,17 +319,27 @@ if __name__ == "__main__":
             else:
                 nx_graph.nodes[node]['type']='user'
         end = time.time()
-        print("Loading time: ", end-start)
+        print("\tLoading time: ", end-start)
     else:
-        print("Category graph not found...generating")
+        print("Generating new category graph (est. ~90 seconds)")
         start = time.time()
         nx_graph = generate_graph()
         end = time.time()
-        print("Generation time: ", end-start)
+        print("\tGeneration time: ", end-start)
 
-    print('Activating StellarGraph Library')
-    stellar_graph = StellarGraph.from_networkx(nx_graph,node_type_attr='type',edge_type_attr='type')
+    print('Activating StellarGraph Library (est. ~30 seconds)')
+    start = time.time()
+    stellar_graph = StellarGraph.from_networkx(nx_graph,node_type_attr='type',edge_type_attr='type', edge_weight_attr='weight')
     print(stellar_graph.info())
+    end = time.time()
+    print("\tActivation time: ", end-start)
+
+    split_train_test(stellar_graph)
+
+
+        
+        
+        
 
     #visualize_graph(graph)
 
